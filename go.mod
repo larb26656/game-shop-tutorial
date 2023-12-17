95e3ed0028c3ac474414818291cb0a4e69e49196
Errortime <larb26656@gmail.com>
2023-12-17 11:27:30 +0700

    Feat (setup config system and test config reader)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/larb26656/game-shop-tutorial
 
 go 1.21.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/larb26656/game-shop-tutorial/config"
+)
+
 func main() {
-	
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize config
+
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		} 
+
+		return os.Args[1]
+	}())
+
+	log.Println(cfg)
 }
