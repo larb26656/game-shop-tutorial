e3dbafc83e16d3e83f30a6da5509c53c0a2ca057
Errortime <larb26656@gmail.com>
2023-12-21 10:41:53 +0700

    Feat (initialize auth grpc handler)

--- modules/auth/authHandler/authGrpcHandler.go
+++ modules/auth/authHandler/authGrpcHandler.go
@@ -1,13 +1,29 @@
 package authHandler
 
-import "github.com/larb26656/game-shop-tutorial/modules/auth/authUsecase"
+import (
+	"context"
+
+	authPb "github.com/larb26656/game-shop-tutorial/modules/auth/authPb"
+	"github.com/larb26656/game-shop-tutorial/modules/auth/authUsecase"
+)
 
 type (
 	authGrpcHandler struct {
+		authPb.UnimplementedAuthGrpcServiceServer
 		authUsecase authUsecase.AuthUsecaseService
 	}
 )
 
-func NewAuthGrpcHandler(authUsecase authUsecase.AuthUsecaseService) authUsecase.AuthUsecaseService {
-	return &authGrpcHandler{authUsecase}
+func NewAuthGrpcHandler(authUsecase authUsecase.AuthUsecaseService) *authGrpcHandler {
+	return &authGrpcHandler{
+		authUsecase: authUsecase,
+	}
+}
+
+func (g *authGrpcHandler) AccessTokenSearch(ctx context.Context, req *authPb.AccessTokenSearchReq) (*authPb.AccessTokenSearchRes, error) {
+	return nil, nil
+}
+
+func (g *authGrpcHandler) RolesCount(ctx context.Context, req *authPb.RolesCountReq) (*authPb.RolesCountRes, error) {
+	return nil, nil
 }
